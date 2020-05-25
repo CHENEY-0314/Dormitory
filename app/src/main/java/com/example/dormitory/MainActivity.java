4585821c81573dcd927ca2b13f6a9306b7bbfd68
CHENEY-0314 <529269793@qq.com>
2020-05-25 14:21:20 +0800

    version0.1

---
+++ app/src/main/java/com/example/dormitory/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dormitory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //彩蛋
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
