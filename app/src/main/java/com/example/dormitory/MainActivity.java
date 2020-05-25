b9d0d2ff56941f7c65f9a8184411eac67f520ed5
hgl <2319100642@qq.com>
2020-05-25 16:36:23 +0800

    version1.0

--- app/src/main/java/com/example/dormitory/MainActivity.java
+++ app/src/main/java/com/example/dormitory/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     //彩蛋
     //彩蛋
     //小海的彩蛋~
-
+    //渣男陈晓姐
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
