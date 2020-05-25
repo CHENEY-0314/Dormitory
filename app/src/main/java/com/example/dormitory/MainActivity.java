5b2b817dbaa863df1677ce291fe0c8688c0c2e21
hgl <2319100642@qq.com>
2020-05-25 16:21:57 +0800

    version1.0

--- app/src/main/java/com/example/dormitory/MainActivity.java
+++ app/src/main/java/com/example/dormitory/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //彩蛋
+    //彩蛋渣男cxj
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
