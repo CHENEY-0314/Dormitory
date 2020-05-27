bb0fca5d16ed51292106ba79f466fa783dfdc953
hgl <2319100642@qq.com>
2020-05-27 23:45:06 +0800

    version1.0

---
+++ app/src/main/java/com/example/dormitory/HomePage.java
@@ -0,0 +1,20 @@
+package com.example.dormitory;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomePage extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.homepage_layout, container, false);
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/dormitory/MainPage.java
@@ -0,0 +1,21 @@
+package com.example.dormitory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainPage extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.mainpage_layout, container, false);
+        return view;
+    }
+
+}
