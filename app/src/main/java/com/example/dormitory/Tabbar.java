674f1ac2abde5afe42a85adb37db2a16d5e9afa0
hgl <2319100642@qq.com>
2020-05-29 12:37:45 +0800

    version

--- app/src/main/java/com/example/dormitory/Tabbar.java
+++ app/src/main/java/com/example/dormitory/Tabbar.java
@@ -20,13 +20,10 @@ public class Tabbar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bottomtabbar_layout);
         initView();
-
         initData();
     }
 
     private void initData() {
-
-        setGestureListener();
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
         //设置状态栏文字颜色及图标为深色，当状态栏为白色时候，改变其颜色为深色，简单粗暴直接完事
@@ -47,46 +44,44 @@ public class Tabbar extends AppCompatActivity {
                     @Override
                     public void onTabChange(int position, String name, View view) {
                         Log.i("TGA", "位置：" + position + "      选项卡的文字内容：" + name);
-//                        mViewPager.setCurrentItem(position);
+                        //mViewPager.setCurrentItem(position);
+
                         overPosition = position;
                     }//添加选项卡切换监听
                 })
-                .setCurrentTab(0);//设置当前选中的tab,从0开始
+                .setCurrentTab(0)//设置当前选中的tab,从0开始
+                .setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                mPosX = event.getX();
+                                mPosY = event.getY();
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+                                mCurPosX = event.getX();
+                                mCurPosY = event.getY();
+                                break;
+                            case MotionEvent.ACTION_UP:
+                                if (mCurPosX - mPosX > 20 &&  (Math.abs(mCurPosX-mPosX) > 25)) {
+                                    //向左滑動
+                                    overPosition-=1;
+                                    bottom_tab_bar.setCurrentTab(overPosition );
+                                    System.out.println(overPosition);
 
-
-    }
-
-    private void setGestureListener() {
-        bottom_tab_bar.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-
-                    case MotionEvent.ACTION_DOWN:
-                        mPosX = event.getX();
-                        mPosY = event.getY();
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        mCurPosX = event.getX();
-                        mCurPosY = event.getY();
-
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        if (mCurPosX - mPosX > 0 && (Math.abs(mCurPosX - mPosX) > 25)) {
-                            //向左滑動
-                            bottom_tab_bar.setCurrentTab(overPosition - 1);
-
-                        } else if (mCurPosX - mPosX < 0 && (Math.abs(mCurPosX - mPosX) > 25)) {
-                            //向右滑动
-                            bottom_tab_bar.setCurrentTab(overPosition + 1);
+                                } else if (mCurPosX - mPosX < 0 && (Math.abs(mCurPosX-mPosX) > 25)) {
+                                    //向右滑动
+                                    overPosition+=1;
+                                    bottom_tab_bar.setCurrentTab(overPosition);
+                                }
+                                if (mCurPosX - mPosX > 0 && overPosition==2)
+                                    bottom_tab_bar.setCurrentTab(1);
+                                break;
                         }
-                        break;
-                }
-                return true;
-            }
-        });
+                        return true;
+                    }
+                });
     }
-
     private void initView() {
         bottom_tab_bar = (BottomTabBar) findViewById(R.id.bottom_tab_bar);
     }
