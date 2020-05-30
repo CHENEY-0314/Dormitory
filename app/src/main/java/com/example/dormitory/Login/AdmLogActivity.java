b4d6c100448991a8e91fbc8020f0393d9eec25da
ZTH-CS <59355019+ZTH-CS@users.noreply.github.com>
2020-05-30 10:08:50 +0800

    写了两个登录页面
    学生+管理员的登录页面
    逻辑上还没实现。

---
+++ app/src/main/java/com/example/dormitory/Login/AdmLogActivity.java
@@ -0,0 +1,32 @@
+package com.example.dormitory.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.dormitory.R;
+import com.example.dormitory.Tabbar;
+
+public class AdmLogActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adm_log);
+        EditText aAccount = findViewById(R.id.adm_account);
+        EditText aPassword = findViewById(R.id.adm_password);
+        Button mBtnALogin = findViewById(R.id.btn_aLogin);
+        mBtnALogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdmLogActivity.this, Tabbar.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(),"登录成功",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/dormitory/LoginActivity.java
+++ app/src/main/java/com/example/dormitory/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.dormitory;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.dormitory.Login.AdmLogActivity;
+import com.example.dormitory.Login.StuLogActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         // 学生登录 + 管理员登录
         Button mBtnStuLog = findViewById(R.id.btn_stuLogin);
         Button mBtnAdmLog = findViewById(R.id.btn_admLogin);
@@ -19,14 +22,16 @@ public class LoginActivity extends AppCompatActivity {
         mBtnStuLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LoginActivity.this,StuLogActivity.class);
+                startActivity(intent);
             }
         });
         // 管理员登录按钮点击事件
         mBtnAdmLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LoginActivity.this, AdmLogActivity.class);
+                startActivity(intent);
             }
         });
     }
