3b351d64059063a3f5c5fe3f2add9fdda0dc9d39
CHENEY-0314 <529269793@qq.com>
2020-05-31 13:09:11 +0800

    更改了首页背景

--- app/src/main/java/com/example/dormitory/Tabbar.java
+++ app/src/main/java/com/example/dormitory/Tabbar.java
@@ -27,8 +27,6 @@ public class Tabbar extends AppCompatActivity{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        //设置状态栏文字颜色及图标为深色，当状态栏为白色时候，改变其颜色为深色，简单粗暴直接完事
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bottomtabbar_layout);
