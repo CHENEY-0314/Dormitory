d16fe47829ae64e725827cf5a1c008a2edc2c486
CHENEY-0314 <529269793@qq.com>
2020-06-01 15:33:01 +0800

    更改了宿舍维修申请页面

--- app/src/main/java/com/example/dormitory/repairDormitory.java
+++ app/src/main/java/com/example/dormitory/repairDormitory.java
@@ -3,12 +3,18 @@ package com.example.dormitory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class repairDormitory extends AppCompatActivity {
     //声明返回按钮的组件
     private ImageView mBack;
+    private TextView mOtherNum;
+    private EditText mEdtOther;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //设置状态栏文字颜色及图标为深色，当状态栏为白色时候，改变其颜色为深色，简单粗暴直接完事
@@ -22,5 +28,20 @@ public class repairDormitory extends AppCompatActivity {
                 repairDormitory.this.finish();
             }
         });
+
+        mOtherNum=findViewById(R.id.repairDor_txt_OtherNum);
+        mEdtOther=findViewById(R.id.repairDor_edt_Other);
+        //监听输入框字数变化
+        mEdtOther.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+            @Override
+            public void afterTextChanged(Editable s) {  //监听输入框字数变化
+                mOtherNum.setText(String.valueOf(s.length())+"/400");
+            }
+        });
+
     }
 }
